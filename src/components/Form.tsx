3ed2fd9af43bed457762cc529a9d87ec05321c83
phiniqq724 <fahrellnew@gmail.com>
2024-04-16 16:31:49 +0700

    hh

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,29 +4,29 @@ const ScriptURL =
   "https://script.google.com/macros/s/AKfycby160jp5aVXOnhFP7WiJVf7CZ3we3jqBVl3eip7qmtTCF8bX__4BhJmBDo4cqkR6g5r/exec";
 
 const Forms: React.FC = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [isMutating, setIsMutating] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isMutating, setIsMutating] = useState(false);
 
-    const hanldeSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const hanldeSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsMutating(true);
     try {
-        const response = await fetch(ScriptURL, {
+      const response = await fetch(ScriptURL, {
         method: "POST",
         body: new FormData(e.currentTarget),
-        });
-        console.log("Success", response);
-        window.location.reload();
-        alert("Terkirim!");
+      });
+      console.log("Success", response);
+      window.location.reload();
+      alert("Terkirim!");
     } catch (error) {
-        console.log("Erroor", (error as Error).message);
+      console.log("Erroor", (error as Error).message);
     }
-    };
-    return (
-        <>
-        <div className="mt-8">
+  };
+  return (
+    <>
+      <div className="mt-8">
         <form action="" className="flex flex-col" onSubmit={hanldeSubmit}>
           <input
             type="text"
@@ -54,13 +54,13 @@ const Forms: React.FC = () => {
           />
           <div className="mt-4">
             {!isMutating ? (
-                <button className=" bg-oranges text-white rounded-xl w-[706px] h-16 text-xl font-raleway font-medium hover:bg-lightorange hover:text-white transition-all duration-500 ">
+              <button className=" bg-oranges text-white rounded-xl w-[706px] h-16 text-xl font-raleway font-medium hover:bg-lightorange hover:text-white transition-all duration-500 ">
                 Submit
-                </button>
+              </button>
             ) : (
-                <button className=" bg-oranges text-white rounded-xl w-[706px] h-16 text-xl font-raleway font-medium hover:bg-lightorange hover:text-white transition-all duration-500 ">
+              <button className=" bg-oranges text-white rounded-xl w-[706px] h-16 text-xl font-raleway font-medium hover:bg-lightorange hover:text-white transition-all duration-500 ">
                 Loading . . .
-                </button>
+              </button>
             )}
           </div>
         </form>
--- src/components/Portofolio.tsx
+++ src/components/Portofolio.tsx
@@ -1,7 +1,7 @@
-import CarouselComp from "./CarouselComp";
 import Sertifikat1 from "../../public/Sertifikat1.png";
 import Smartaca from "../../public/Smartaca.png";
 import PyroEyes from "../../public/PyroEyes.jpg";
+import ButtonBlack from "./ButtonBlack";
 
 export default function Portofolio() {
   return (
@@ -14,12 +14,27 @@ export default function Portofolio() {
             Certificate & Projects
           </h1>
         </div>
-        <div className=" w-[875px] h-[545px] mt-8 mb-64 ">
-          <CarouselComp
-            Source1={Sertifikat1}
-            Source2={Smartaca}
-            Source3={PyroEyes}
-          />
+        <div className="w-[1500px] mt-8 mb-64 flex">
+          <a href="#" className=" w-[575px] h-[245px]">
+            <img
+              src={Sertifikat1}
+              alt="Sertifikat IoT"
+              className="rounded-xl "
+            />
+          </a>
+          <a href="#" className=" w-[575px] h-[245px]">
+            <img src={Smartaca} alt="Project 1" className="rounded-xl" />
+          </a>
+          <a href="#" className=" w-[575px] h-[245px]">
+            <img src={PyroEyes} alt="Project 2" className="rounded-xl" />
+          </a>
+        </div>
+        <div className="justify-center items-center flex ">
+          <a href="#" className="">
+            <button className=" bg-darkaccent text-white rounded-xl w-48 h-16 text-xl font-nunito transition-colors duration-250 hover:bg-oranges hover:text-white">
+              See More
+            </button>
+          </a>
         </div>
       </div>
     </div>
